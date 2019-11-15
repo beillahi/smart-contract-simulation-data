5a78515dcaf8396452cfdcb87edcfd104e9b4069
Michael Emmi <michael.emmi@gmail.com>
2019-11-15 10:44:39 -0500

    Polished data generation script.

--- bin/generate-experiment-data.sh
+++ bin/generate-experiment-data.sh
@@ -1,39 +1,68 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
-eval "cd .."
-CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PATH_to_CONFIG="${CURRENTDIR}/configs" 
+ROOT="$(cd $(dirname $(dirname $BASH_SOURCE[0])) && pwd)"
+CONFIGS="$ROOT/configs"
+CONTRACTS="$ROOT/contracts"
+IGNORED_KEYS="description"
+COMMAND="sc-simulation"
+TIMESTAMP=$(date +"%Y:%m:%d:%H:%M:%S")
+OUTPUT="$(pwd)/output:${TIMESTAMP}"
+DRYRUN=1
 
-for file in "${PATH_to_CONFIG}"/*; do
-    cmd_config="sc-simulation "
-    Full_filename="${file}" 
-    SOURCE=$(eval "jq .source ${Full_filename}")
-    SOURCE="${SOURCE%\"}"
-    SOURCE="${SOURCE#\"}"
-    [ "$SOURCE" == null ]  || cmd_config="${cmd_config} --source ${CURRENTDIR}/contracts/${SOURCE}"
-    TARGET=$(eval "jq .target ${Full_filename}")
-    TARGET="${TARGET%\"}"
-    TARGET="${TARGET#\"}"
-    [ "$TARGET" == null ] || cmd_config="${cmd_config} --target ${CURRENTDIR}/contracts/${TARGET}"
-    STATES=$(eval "jq .states ${Full_filename}")
-    [ "$STATES" == null ] || cmd_config="${cmd_config} --states ${STATES}"
-    BALANCES=$(eval "jq .balances ${Full_filename}")
-    [ "$BALANCES" == null ] || cmd_config="${cmd_config} --balances ${BALANCES}"
-    PAYMENTS=$(eval "jq .payments ${Full_filename}")
-    [ "$PAYMENTS" == null ] || cmd_config="${cmd_config} --payments ${PAYMENTS}"
-    INTEGERS=$(eval "jq .integers ${Full_filename}")
-    [ "$INTEGERS" == null ] || cmd_config="${cmd_config} --integers ${INTEGERS}"
-    UNSIGNEDINTEGERS=$(eval "jq .unsignedintegers ${Full_filename}")
-    [ "$UNSIGNEDINTEGERS" == null ] || cmd_config="${cmd_config} --unsigned-integers ${UNSIGNEDINTEGERS}"
-    BYTES=$(eval "jq .bytes ${Full_filename}")
-    [ "$BYTES" == null ] || cmd_config="${cmd_config} --bytes ${BYTES}"
-    NUMACCOUNTS=$(eval "jq .numaccounts ${Full_filename}")
-    [ "$NUMACCOUNTS" == null ] || cmd_config="${cmd_config} --num-accounts ${NUMACCOUNTS}"
-    NUMSENDERS=$(eval "jq .numsenders ${Full_filename}")
-    [ "$NUMSENDERS" == null ] || cmd_config="${cmd_config} --num-senders ${NUMSENDERS}"
-    NUMEXAMPLES=$(eval "jq .numexamples ${Full_filename}")
-    [ "$NUMEXAMPLES" == null ] || cmd_config="${cmd_config} --num-examples ${NUMEXAMPLES}"
-    eval $cmd_config
+mkdir -p $OUTPUT
+
+echo Generating experimental data to $OUTPUT
+echo ---
+
+for file in $(find $CONFIGS -name "*.json")
+do
+    name=$(basename $file .json)
+    output="$OUTPUT/$name"
+    command="$COMMAND --output $output"
+
+    echo Processing example: $name
+    echo ---
+
+    # consider any fields defined in the given config
+    for key in $(jq -r 'keys[]' $file)
+    do
+        # read the value of the given key
+        value=$(jq -r .${key//-/} $file)
+
+        # skip ignored keys and null values
+        if [[ " $IGNORED_KEYS " =~ " $key " || "$value" == "null" ]]
+        then
+            continue
+        fi
+
+        # prepend the contracts path contract-path values
+        if [[ "$value" =~ .sol$ ]]
+        then
+            value="$CONTRACTS/$value"
+        fi
+
+        # just generate files but don’t actually do much
+        if [[ -n $DRYRUN ]]
+        then
+            command="$command --no-examples --no-synthesize --no-verify"
+        fi
+
+        # append the key to the command
+        command="$command --$key $value"
+    done
+
+    # actually run the command
+    echo Running command: $command
+    echo ---
+    mkdir -p "$output"
+    $command > >(tee $output/stdout.log) 2> >(tee $output/stderr.log >&2)
+
+    echo ---
 done
+
+echo Compressing the results into $OUTPUT.zip
+echo ---
+zip -r "$OUTPUT.zip" "$OUTPUT"
+echo ---
