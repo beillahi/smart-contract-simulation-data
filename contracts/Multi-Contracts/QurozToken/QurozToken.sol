d0b0856fedb52f514491310346d3d0411ef41e6f
beillahi <beillahi@irif.fr>
2019-11-22 17:26:57 +0100

    Rollback the new updates

--- contracts/Multi-Contracts/QurozToken/QurozToken.sol
+++ contracts/Multi-Contracts/QurozToken/QurozToken.sol
@@ -86,34 +86,35 @@ contract QurozToken {
     allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
     _burn(_from, _value);
   }
-  
-  function approve(address _spender, uint256 _value) public whenNotPaused {
+ 
+  function approve(address _spender, uint256 _value) public whenNotPaused returns (bool) {
       //StandardToken.sol, PausableToken.sol
     allowed[msg.sender][_spender] = _value;
     emit Approval(msg.sender, _spender, _value);
+    return true;
   }
 
   function allowance(address _owner, address _spender) public view returns (uint256) { //StandardToken.sol
     return allowed[_owner][_spender];
   }
 
-  function increaseApproval(address _spender, uint256 _addedValue) public whenNotPaused returns(bool){
+  function increaseAllowance(address _spender, uint256 _addedValue) public whenNotPaused  { //returns(bool){
       //StandardToken.sol, PausableToken.sol
     allowed[msg.sender][_spender] = (allowed[msg.sender][_spender].add(_addedValue));
     emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
+    //return true;
   }
 
-  function decreaseApproval(address _spender, uint256 _subtractedValue) public whenNotPaused returns(bool) {
+  function decreaseAllowance(address _spender, uint256 _subtractedValue) public whenNotPaused { //returns(bool) {
       //StandardToken.sol, PausableToken.sol
     uint256 oldValue = allowed[msg.sender][_spender];
     if (_subtractedValue > oldValue) { allowed[msg.sender][_spender] = 0;
     } else                           { allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);}
     emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
+    //return true;
   }
 
-  function transfer(address _to, uint256 _value) public whenNotPaused  {//BasicToken.sol, PausableToken.sol
+  function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {//BasicToken.sol, PausableToken.sol
     require(_to != address(0));
     require(_value <= balances[msg.sender]);
     require(locked[msg.sender] != true);                                                             // new
@@ -121,9 +122,10 @@ contract QurozToken {
     balances[msg.sender] = balances[msg.sender].sub(_value);
     balances[_to] = balances[_to].add(_value);
     emit Transfer(msg.sender, _to, _value);
+    return true;
   }
 
-  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused  {  
+  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {  
       //StandardToken.sol, PausableToken.sol
     require(_to != address(0));
     require(_value <= balances[_from]);
@@ -134,7 +136,9 @@ contract QurozToken {
     balances[_to] = balances[_to].add(_value);
     allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
     emit Transfer(_from, _to, _value);
+    return true;
   }
+
 //
   /**       @notice modifies owner
   */
